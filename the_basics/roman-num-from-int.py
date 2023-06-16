a327052f8a64c7ab50997ca33117e5e39a5562a9
tfrum <119856929+tfrum@users.noreply.github.com>
2023-06-15 23:03:35 -0600

    Update roman-num-from-int.py

--- the_basics/roman-num-from-int.py
+++ the_basics/roman-num-from-int.py
@@ -7,7 +7,7 @@ ronum_dict = {
     "D": 500,
     "M": 1000
 }
-intnum = 1994
+intnum = 3999
 
 
 
@@ -19,12 +19,19 @@ def get_numeral(num):
 
 
 def break_down(num):
+    return_string = ""
+
     if num in ronum_dict.values():
         return get_numeral(num)
-    else:
+    elif num <= 1000:
         for key, value in ronum_dict.items():
             if num < value:
                 return get_numeral(value - num) + str(key)
+    elif num >= 1000:
+        m_ronums = num // 1000
+        for i in range(m_ronums):
+            return_string += "M"
+        return return_string
         
 
 
@@ -37,7 +44,8 @@ def main():
         # this gets us whole numbers in each place of the number
 
         temp_int = opnum - (opnum % (10**(len_num - i - 1)))
-        output += break_down(temp_int)
+        if temp_int != 0:
+            output += break_down(temp_int)
         opnum -= temp_int
 
     print(output)
